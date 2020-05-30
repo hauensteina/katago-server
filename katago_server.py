bb145d62c8d28a042cdab14a7a0a465c055abb17
Andreas Hauenstein <hauensteina@gmail.com>
2020-05-30 20:48:31 +0000

    switch to 20 block net

--- katago_server.py
+++ katago_server.py
@@ -33,7 +33,7 @@ from encoder_base import get_encoder_by_name
 from scoring import compute_nn_game_result
 
 
-katago_cmd = './katago gtp -model g170-b40c256x2-s3708042240-d967973220.bin.gz -config gtp_ahn.cfg '
+katago_cmd = './katago gtp -model g170e-b20c256x2-s4384473088-d968438914.bin.gz -config gtp_ahn.cfg '
 katago_gtp_bot = KataGTPBot( katago_cmd.split() )
 
 # Get an app with 'select-move/<botname>' endpoints
